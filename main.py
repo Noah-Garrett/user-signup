ee5b760e4af07619cb899837eb2163f2895a79dd
Noah Garrett <garretns@gmail.com>
2019-12-05 00:35:51 -0600

    good2go

--- main.py
+++ main.py
@@ -9,32 +9,59 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), a
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/")
-def index():
-    return render_template("main_form.html", username_error='',password_error='',password_match='', email_error='')
-
-
-@app.route('/', methods=['POST'])
-def error():
+@app.route("/" ,methods=['POST','GET'])
+def form():
     username_error=''
     password_error=''
     password_match=''
     email_error=''
+
+    if request.method=='GET':
+        return render_template('main_form.html')
+    else:
+        username = request.form['username']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        email = request.form['email']
+
+        something_wrong=True
+
+        #blank checks $$$
+        if username == '':
+            username_error='please enter a username'
+            something_wrong=False
+        if password == '':
+            password_error='please enter a password'
+            something_wrong=False
+        if confirm_password == '':
+            password_match='please confirm password'
+            something_wrong=False
+
+        #fancy boi checks 
+        if len(username) <=3 and len(username)>0 or len(username)>20 or ' ' in username:
+            username_error='that username is is invalid'
+            something_wrong=False
+        
+        if password != confirm_password:
+            password_match="passwords do not match"
+            something_wrong=False
+
+        #email oh lawd
+        if email != '':
+            if '@'  not in email or  '.'  not in email:
+                email_error='please enter a valid email'
+                something_wrong=False
+
+         #if it's all good you're clear
+        if something_wrong==True:
+            return redirect('/success?user=' + username)
+
+        return render_template("main_form.html",username_error = username_error,password_error = password_error,password_match= password_match,email_error=email_error, email=email, username=username)
     
-    username = request.form['username']
-
-    if username == '':
-        username_error='please enter a username'
-    #elif password == '':
-        #password_error='please enter a password'
-    #elif confirm_password == '':
-        #password_match='please confirm password'
-
-    if len(username) <=3 and len(username)>0:
-        username_error='that username is too short'
-
-    return render_template("main_form.html",username_error = username_error,password_error = password_error,password_match= password_match,email_error=email_error)
-    
+@app.route('/success')
+def success():
+    username=request.args.get('user')
+    return render_template("logged_in.html", username=username)
 
 
 app.run()
