a4c07f27d9d38f5575dbc714b1a14323217ce6af
Noah Garrett <garretns@gmail.com>
2019-12-06 13:59:23 -0600

    swapped true and false

--- main.py
+++ main.py
@@ -25,40 +25,40 @@ def form():
         confirm_password = request.form['confirm_password']
         email = request.form['email']
 
-        something_wrong=True
+        something_wrong=False
 
         #blank checks $$$
         if username == '':
             username_error='please enter a username'
-            something_wrong=False
+            something_wrong=True
         if password == '':
             password_error='please enter a password'
-            something_wrong=False
+            something_wrong=True
         if confirm_password == '':
             password_match='please confirm password'
-            something_wrong=False
+            something_wrong=True
 
         #fancy boi checks 
-        if len(username) <=3 and len(username)>0 or len(username)>20 or ' ' in username:
-            username_error='that username is is invalid'
-            something_wrong=False
+        if len(username) <3 and len(username)>0 or len(username)>20 or ' ' in username:
+            username_error='username cannot have space or less than 3'
+            something_wrong=True
 
-        if len(password) <=3 and len(password)>0 or len(password)>20 or ' ' in password:
+        if len(password) <3 and len(password)>0 or len(password)>20 or ' ' in password:
             password_type_error='passwords cannot contain spaces or be less than 3'
-            something_wrong=False
+            something_wrong=True
         
         if password != confirm_password:
             password_match="passwords do not match"
-            something_wrong=False
+            something_wrong=True
 
         #email oh lawd
         if email != '':
             if '@'  not in email or  '.'  not in email:
                 email_error='please enter a valid email'
-                something_wrong=False
+                something_wrong=True
 
          #if it's all good you're clear
-        if something_wrong==True:
+        if something_wrong==False:
             return redirect('/success?user=' + username)
 
         return render_template("main_form.html",username_error = username_error,password_error = password_error,password_match= password_match,email_error=email_error, email=email, username=username, password_type_error=password_type_error)
